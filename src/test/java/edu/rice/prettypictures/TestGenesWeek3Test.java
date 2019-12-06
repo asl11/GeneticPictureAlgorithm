811f9dfa6d9fe66a237e4666f0cd1d44f274b812
Josh <dongjoshua@yahoo.com>
2019-12-05 20:29:18 -0600

    Updated Typos and Google Java Format

--- src/test/java/edu/rice/prettypictures/TestGenesWeek3Test.java
+++ src/test/java/edu/rice/prettypictures/TestGenesWeek3Test.java
@@ -1,11 +1,13 @@
 package edu.rice.prettypictures;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 import io.vavr.collection.Seq;
 import java.awt.image.BufferedImage;
 import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 
 public class TestGenesWeek3Test {
@@ -17,6 +19,7 @@ public class TestGenesWeek3Test {
     var img1 = RandomGeneTree.randomTree(5);
     var img2 = RandomGeneTree.randomTree(5);
     Seq<GeneTree> mutatedTrees = new TestGenesWeek3(img1, img2, 30).getGenes();
+    System.out.println(mutatedTrees);
     return mutatedTrees.map(
         gene ->
             dynamicTest(
@@ -61,6 +64,21 @@ public class TestGenesWeek3Test {
                 }));
   }
 
+  @Test
+  void testMutateHelper() {
+    // Test that mutate works and that the trees are changed at worst case at least 80% of the time
+    Seq<GeneTree> testTrees = TestGenesWeek2.randomTrees(30);
+    Seq<GeneTree> mutatedTrees = testTrees.map(GeneTree::mutateTree);
+    int count = 0;
+    for (int i = 0; i < testTrees.length(); i++) {
+      if (!testTrees.get(i).equals(mutatedTrees.get(i))) {
+        count++;
+      }
+    }
+
+    assertTrue(count / (double) testTrees.length() > .8);
+  }
+
   @TestFactory
   Seq<DynamicTest> testTest4TreesAreWorking() {
     var img1 = RandomGeneTree.randomTree(5);
@@ -76,5 +94,21 @@ public class TestGenesWeek3Test {
                   assertEquals(150, result.getWidth());
                   assertEquals(100, result.getHeight());
                 }));
+  }
+
+  @Test
+  void testBreedHelper() {
+    // Test that breed works and that the trees are changed at worst case at least 80% of the time
+    Seq<GeneTree> testTrees = TestGenesWeek2.randomTrees(30);
+    Seq<GeneTree> testTwoTrees = TestGenesWeek2.randomTrees(1);
+    Seq<GeneTree> breededTrees = testTrees.map(tree -> tree.crossBreed(testTwoTrees.get(0)));
+    int count = 0;
+    for (int i = 0; i < testTrees.length(); i++) {
+      if (!testTrees.get(i).equals(breededTrees.get(i))) {
+        count++;
+      }
+    }
+
+    assertTrue((count / (double) testTrees.length()) > .8);
   }
 }
