8eaa965d24ec6ea0701c229ef089634966d7144c
asl11 <asl11@rice.edu>
2020-08-19 18:25:58 -0700

    Made project structure clearer

--- rungrader.sh
+++
@@ -1,7 +0,0 @@
-# this script avoids a weird problem where Gradle is printing the autograder output at the wrong time
-
-export RICECHECKS_QUIET=true
-./gradlew --console=plain autograder
-retVal=$?
-cat build/autograder/report.txt
-exit $retVal
--- src/main/resources/cool-images/README.txt
+++
@@ -1,3 +0,0 @@
-Sources for cool pictures:
-    Library of Congress (www.loc.gov/pictures)
-    NASA (photojournal.jpl.nasa.gov) and (images.nasa.gov)
